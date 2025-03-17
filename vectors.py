8d99417290411b79a0b9956a485f561e2500f90d
atulyatushar <atulyatushar@gmail.com>
2025-03-17 23:35:50 +0530

    vector sample 1

---
+++ vectors.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 16 12:27:25 2025
+
+@author: atuly
+"""
+
+import numpy as np  # Import NumPy
+
+x = np.linspace(0, 10, 10)  # Generate 10 evenly spaced values from 0 to 10
+print(x)
+
+y = np.arange(0, 10, 0.3)  # Generate values from 0 to <10 with a step of 0.3
+print(y)
+
+
+
+
+
