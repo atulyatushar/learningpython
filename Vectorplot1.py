7dcba8e9c71d6ce9b21d02b91ebfff9a58594783
atulyatushar <atulyatushar@gmail.com>
2025-03-17 23:32:50 +0530

    vector sample 1

---
+++ Vectorplot1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 16 23:57:29 2025
+
+@author: atuly
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, 10, 10)
+y = np.sin(x)
+plt.plot(x,y)
+plt.show()
